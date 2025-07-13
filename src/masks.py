7921515f21b37617d656bee0b0ab8974c08e700b
Kosarew Konstantin <kos26193@gmail.com>
2025-07-14 00:14:30 +0300

    добавлено логирование в файлы masks.py, utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,14 @@
+import logging
+
+#описание логера
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("..\\logs\\masks.log", "w+", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 INVALID_CARD_NUMBER = "Неверный номер карты!"
 
 
@@ -16,8 +27,9 @@ def get_mask_card_number(number_card: str) -> str:
     digits = "".join(char for char in number_card if char.isdigit())
 
     if len(digits) != 16:
+        logger.error(f"\nИмя файла: {__name__}, имя функции get_mask_card_number - {INVALID_CARD_NUMBER}")
         raise ValueError(INVALID_CARD_NUMBER)
-
+    logger.info(f"\nИмя файла: {__name__}, имя функции get_mask_card_number - Ок")
     return f"{digits[0:4]} {digits[4:6]}** **** {digits[-4:]}"
 
 
@@ -34,5 +46,7 @@ def get_mask_account(account_number: str) -> str:
     """
     digits = "".join(char for char in account_number if char.isdigit())
     if len(digits) != 20:
+        logger.error(f"\nИмя файла: {__name__}, имя функции get_mask_account - {INVALID_CARD_NUMBER}")
         raise ValueError(INVALID_CARD_NUMBER)
+    logger.info(f"\nИмя файла: {__name__}, имя функции get_mask_account - Ok")
     return f"**{digits[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
+import logging
 from pathlib import Path
+
+# описание логера
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("..\\logs\\utils.log", "w+", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 # currency_converter
@@ -20,6 +29,7 @@ def transaction_loader(file_path: str = "") -> list:
     path = Path(file_path)
     # Проверка существования файла
     if not path.is_file():
+        logger.error(f"\nИмя: {__name__} transaction_loader - нет имени файла!")
         return []
 
     try:
@@ -27,13 +37,16 @@ def transaction_loader(file_path: str = "") -> list:
             data = json.load(file)
 
         if isinstance(data, list):
+            logger.info(f"\nИмя: {__name__} transaction_loader - Ok!")
             return data
         return []
 
     except (json.JSONDecodeError, UnicodeDecodeError):
         # Обработка ошибок: пустой файл, битый JSON или проблемы с кодировкой
+        logger.debug(f"\nИмя: {__name__} transaction_loader - пустой файл, битый JSON или проблемы с кодировкой!")
         return []
 
 
 if __name__ == "__main__":
     print(transaction_loader("..\\data\\operations.json"))
+    # print(transaction_loader("..\\data\\operations.jsn"))
